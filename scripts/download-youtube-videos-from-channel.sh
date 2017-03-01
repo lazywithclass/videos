5debee9b94983342341138ee91cd93f9e444c577
lazywithclass <montecristoh@gmail.com>
2017-03-01 11:39:01 -0500

    fixed path

--- scripts/download-youtube-videos-from-channel.sh
+++ scripts/download-youtube-videos-from-channel.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./youtube-dl -o './%(playlist)s/%(chapter_number)s - %(chapter)s/%(title)s.%(ext)s' ytuser:$1
+./scripts/youtube-dl -o './%(playlist)s/%(chapter_number)s - %(chapter)s/%(title)s.%(ext)s' ytuser:$1
